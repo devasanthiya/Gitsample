881dd17c09a8d4878e019a1c905dce1cb65c23d5
devasanthiya <devajobhunter@gmail.com>
2021-01-11 22:03:52 +0530

    to branch2

--- src/prog1.java
+++ src/prog1.java
@@ -1,8 +1,13 @@
 
 public class prog1 {
+	public void sub() {
+		System.out.println("sub");
 
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		prog1 p=new prog1();
+		p.sub();
 		System.out.println(10);
 		System.out.println(20);
 		System.out.println(30);
